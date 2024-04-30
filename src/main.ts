cc4d55096d5ca1a803990dce4d223e1fb773281f
VladislavT <vt@burno.tech>
2024-04-30 23:04:26 +0300

    Add sections

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-import './style.css'
+import './reset.css'
+import './style.scss'
+
+import './sections/header/header.scss'
+import './sections/main/main.scss'
+import './sections/how-work/how-work.scss'
+import './sections/about-us/about-us.scss'

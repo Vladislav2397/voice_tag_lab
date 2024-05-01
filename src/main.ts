c0a3e85df8387ad0ed77aec6449bc43bdb913c9d
VladislavT <vt@burno.tech>
2024-05-01 08:51:24 +0300

    Add 'Tags samples' section

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './reset.css'
 import './style.scss'
 
+// Components
+import './components/button/button.scss'
+
+// Sections
 import './sections/header/header.scss'
 import './sections/main/main.scss'
 import './sections/how-work/how-work.scss'
 import './sections/about-us/about-us.scss'
+import './sections/samples/samples.scss'

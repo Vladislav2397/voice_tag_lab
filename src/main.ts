247cd194114cf7f95379f8ec90979f9ddc37febe
VladislavT <vt@burno.tech>
2024-05-01 13:02:21 +0300

    Add more sections to project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.scss'
 
 // Components
 import './components/button/button.scss'
+import './components/review/review.scss'
 
 // Sections
 import './sections/header/header.scss'
@@ -10,3 +11,7 @@ import './sections/main/main.scss'
 import './sections/how-work/how-work.scss'
 import './sections/about-us/about-us.scss'
 import './sections/samples/samples.scss'
+import './sections/futures/futures.scss'
+import './sections/offer/offer.scss'
+import './sections/pricing/pricing.scss'
+import './sections/reviews/reviews.scss'

63273624dff6719e92789959c627d3f3a91e56af
VladislavT <vt@burno.tech>
2024-05-01 20:24:30 +0300

    Make all sections and extract components

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './style.scss'
 // Components
 import './components/button/button.scss'
 import './components/review/review.scss'
+import './components/the-text/the-text.scss'
 
 // Sections
 import './sections/header/header.scss'
@@ -15,3 +16,6 @@ import './sections/futures/futures.scss'
 import './sections/offer/offer.scss'
 import './sections/pricing/pricing.scss'
 import './sections/reviews/reviews.scss'
+import './sections/faq/faq.scss'
+import './sections/have-questions-form/have-questions-form.scss'
+import './sections/footer/footer.scss'
